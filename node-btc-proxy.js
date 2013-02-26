8092598a2b6d4855b43a4045109710050131a1a4
orre <oskar.hane@gmail.com>
2013-02-26 09:28:05 +0100

    Added Copyright definition

--- node-btc-proxy.js
+++ node-btc-proxy.js
@@ -1,3 +1,10 @@
+/*
+Copyright © 2013 Oskar Hane <dev@oskarhane.com>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
+*/
+
 var fs = require('fs');
 var https = require('https');
 var http = require('http');
